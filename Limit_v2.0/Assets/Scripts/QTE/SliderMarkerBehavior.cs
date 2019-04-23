85bd08fa4c81089293455e96ff43d398f4b3ca2a
Andr3w0w3n <epicandr3w@gmail.com>
2019-04-23 11:17:56 -0600

    Jumping Animation Change and Slider Update

---
+++ Limit_v2.0/Assets/Scripts/QTE/SliderMarkerBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderMarkerBehavior : MonoBehaviour
+{
+    private bool goDown = true;
+    private float timer = 0;
+
+    public float moveDownSpeed = 0.005f;
+    public float keyJump = .3f;
+    public float timeActive = 30;
+    // low: -1.7  High: 5.45
+    void FixedUpdate()
+    {
+        if(transform.position.x <= -1.7f)
+        {
+            goDown = false;
+        }
+        else
+        {
+            goDown = true;
+        }
+
+        if (goDown)
+        {
+            transform.position = new Vector2(transform.position.x - moveDownSpeed, transform.position.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.x < 5.45f)
+        {
+            transform.position = new Vector2(transform.position.x + keyJump, transform.position.y);
+        }
+    }
+}
