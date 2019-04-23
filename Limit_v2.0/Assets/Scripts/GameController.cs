32474265564b88780ccaa471c4615a8199444ad1
Andr3w0w3n <epicandr3w@gmail.com>
2019-04-22 21:30:35 -0600

    QTE Update

--- Limit_v2.0/Assets/Scripts/GameController.cs
+++ Limit_v2.0/Assets/Scripts/GameController.cs
@@ -13,7 +13,6 @@ public class GameController : MonoBehaviour
     public bool isAlive = true;
     public static GameController instance;
     public int collected = 0;
-    public int quickTimeScore = 0;
     public float speed = 1;
     public float attackSpeed = 1;
     public float timeSlowSpeed = .50f;
@@ -22,6 +21,16 @@ public class GameController : MonoBehaviour
     public Transform playerPos;
     public Transform cameraPos;
     public bool meteorClose = false;
+
+    public bool isQTE = false;
+    public int quickTimeScore = 0;
+    public int goalScore = 50;
+    private float timer = 0;
+    public float nextLevelTime = 1;
+
+    public static bool gotNPC = false;
+
+    public GameObject nPC;
 
     //awake method
     void Awake()
@@ -33,6 +42,16 @@ public class GameController : MonoBehaviour
         else if (instance != null)
         {
             Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("LevelTwo"))
+        {
+            if(gotNPC == true)
+            {
+                GameObject NPC = Instantiate(nPC, playerPos.position, Quaternion.identity);
+            }  
         }
     }
 
@@ -47,47 +66,64 @@ public class GameController : MonoBehaviour
 
     void Update()
     {
-        //exit game if escape key is pressed
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (!isQTE)
         {
-            Application.Quit();
-        }
-
-        //time slow activation
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            timeSlowActive = !timeSlowActive;
-        }
-        if (timeSlowActive)
-        {
-            timeValue -= .1f;
-            speed = timeSlowSpeed;
-            attackSpeed = attackSpeed+(attackSpeed*timeSlowSpeed);
-            if(timeValue <= 0)
+            //exit game if escape key is pressed
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                timeSlowActive = false;
+                Application.Quit();
+            }
+
+            //time slow activation
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                timeSlowActive = !timeSlowActive;
+            }
+            if (timeSlowActive)
+            {
+                timeValue -= .1f;
+                speed = timeSlowSpeed;
+                attackSpeed = attackSpeed + (attackSpeed * timeSlowSpeed);
+                if (timeValue <= 0)
+                {
+                    timeSlowActive = false;
+                }
+            }
+            else
+            {
+                speed = 1;
+                attackSpeed = 1;
+                if (timeValue < 50)
+                {
+                    timeValue += .01f;
+                }
+            }
+
+            //check to see if player is alive
+            if (lifeValue <= 0)
+            {
+                isAlive = false;
+            }
+
+            //quit game
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
             }
         }
         else
         {
-            speed = 1;
-            attackSpeed = 1;
-            if (timeValue < 50)
+            timer += Time.deltaTime;
+            if(timer >= nextLevelTime)
             {
-                timeValue += .01f;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            print(quickTimeScore);
+            if(quickTimeScore >= goalScore)
+            {
+                gotNPC = true;
             }
         }
-
-        //check to see if player is alive
-        if(lifeValue <= 0)
-        {
-            isAlive = false;
-        }
-
-        //quit game
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+        
     }
 }
