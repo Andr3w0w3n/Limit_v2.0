d7d543960b539e7946a0a177ef741582ea967e54
Andrew Owen <epicandr3w@gmail.com>
2019-04-30 20:59:16 -0600

    Update BossBehavior.cs

--- Limit_v2.0/Assets/Scripts/Enemies/BossBehavior.cs
+++ Limit_v2.0/Assets/Scripts/Enemies/BossBehavior.cs
@@ -9,29 +9,32 @@ public class BossBehavior : MonoBehaviour
     private bool playerAtLeft = false;
     private bool playerAtGround = false;
     private bool playerAtMid = false;
+
+    /*may not need these variables
+    private bool attackRight = false;
+    private bool attackLeft = false;
+    private bool attackMid = false;
+    private bool attackGround = false;
+    */
+
     private bool activeTimer = false;
     private bool isAttacking = false;
     private bool isInPosition = false;
     private bool isBasicAttacking = false;
     private bool isStartingAttacking = false;
-    private bool attack = false;
     private bool isDrained = false;
     private bool playerHitBoss = false;
 
     //Major attack public variables
     public float sideWaitTime = 2;
-    public float midWaitTime = 1;
+    public float midWaitTime = 3;
     public float groundWaitTime = 5;
     public float attackRightXPos;
     public float attackLeftXPos;
     public float attackGroundXPos;
     public float attackMidXPos;
     public float stamina = 100;
-    public float groundAnimationAttackWaitTime = 3;
-    public float midAnimationAttackWaitTime = 3;
-    public float sideAnimationAttackWaitTime = 3;
     public float drainedWaitTime = 5;
-    public float drainedReboundTime = 3;
 
     //major attack viewfinders
     public Viewfinder rightView;
@@ -93,188 +96,20 @@ public class BossBehavior : MonoBehaviour
         //drained stage (after stamina drain)
         if (isDrained)
         {
-            print("oh no im drained");
-            activeTimer = true;
-            if (drainedRebound)
-            {
-                if (timer >= drainedReboundTime)
-                {
-                    print("im rebounding for drainage");
-                    drainedRebound = false;
-                    activeTimer = false;
-                    timer = 0;
-                    isDrained = false;
-                }
-            }
-            else if (timer >= drainedWaitTime)
-            {
-                drainedRebound = true;
-                stamina = 100;
-                timer = 0;
-            }
-            else if (playerHitBoss)
-            {
-
-            }
-            else
-            {
-                print("something is broken in drained");
-            }
+            
         }
 
         //attacking Stage
-        else if (!isStartingAttacking && !isBasicAttacking && isAttacking)
+        else if (isAttacking)
         {
-            print("now im attacking");
-            activeTimer = true;
-            if (isInPosition)
-            {
-                //play animation and wait until animation is over, once animation is over, then
-                if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") || anim.GetCurrentAnimatorStateInfo(0).IsName("Walking"))
-                {
-                    isAttacking = false;
-                }
-                if (stamina <= 0)
-                {
-                    isDrained = true;
-                }
-            }
-            else if (playerAtLeft)
-            {
-                if (timer >= sideWaitTime)
-                {
-                    isInPosition = true;
-                    isAttacking = false;
-                    activeTimer = false;
-                    timer = 0;
-                    stamina -= 10;
-                }
-            }
-            else if (playerAtRight)
-            {
-                if (timer >= sideWaitTime)
-                {
-                    isInPosition = true;
-                    isAttacking = false;
-                    activeTimer = false;
-                    timer = 0;
-                    stamina -= 10;
-                }
-            }
-            else if (playerAtMid)
-            {
-                if (timer >= midWaitTime)
-                {
-                    isInPosition = true;
-                    isAttacking = false;
-                    activeTimer = false;
-                    timer = 0;
-                    stamina -= 25;
-                }
-            }
-            else if (playerAtGround)
-            {
-                if (timer >= groundWaitTime)
-                {
-                    isInPosition = true;
-                    isAttacking = false;
-                    activeTimer = false;
-                    timer = 0;
-                    stamina -= 15;
-                }
-            }
-            else
-            {
-                print("Something is broken in attack");
-            }
+            
         }
 
         //starting attack stage
-        else if (isStartingAttacking && !isBasicAttacking && !isAttacking)
+        else if (isStartingAttacking)
         {
-            print("now im starting to attack because you are in my swamp");
-            if (playerAtLeft)
-            {
-                print("am i making it here??");
-                if (transform.position.x > attackLeftXPos + 1)
-                {
-                    moveLeft(true);
-                }
-                else if (transform.position.x < attackLeftXPos - 1)
-                {
-                    moveRight(true);
-                }
-                else
-                {
-                    isAttacking = true;
-                    movementVector = new Vector2(0, 0);
-                    isStartingAttacking = false;
-                }
-            }
-            else if (playerAtRight)
-            {
-                print("what about here??");
-                if (transform.position.x > attackRightXPos + 1)
-                {
-                    moveLeft(true);
-                }
-                else if (transform.position.x < attackRightXPos - 1)
-                {
-                    moveRight(true);
-                }
-                else
-                {
-                    isAttacking = true;
-                    movementVector = new Vector2(0, 0);
-                    isStartingAttacking = false;
-                }
-            }
-            else if (playerAtMid)
-            {
-                anim.SetBool("Sprint", true);
-                print("here??");
-                if (transform.position.x > attackMidXPos + 1)
-                {
-                    moveLeft(true);
-                }
-                else if (transform.position.x < attackMidXPos - 1)
-                {
-                    moveRight(true);
-                }
-                else
-                {
-                    anim.SetBool("Sprint", false);
-                    anim.SetBool("AttackSide", true);
-                    isAttacking = true;
-                    movementVector = new Vector2(0, 0);
-                    isStartingAttacking = false;
-                }
-            }
-            else if (playerAtGround) //may make this so it does not have a designated position
-            {
-                anim.SetBool("Sprint", true);
-                print("how about here??");
-                if (transform.position.x > attackGroundXPos + 1)
-                {
-                    moveLeft(true);
-                }
-                else if (transform.position.x < attackGroundXPos - 1)
-                {
-                    moveRight(true);
-                }
-                else
-                {
-                    anim.SetBool("Sprint", false);
-                    anim.SetBool("Charge", true);
-                    movementVector = new Vector2(0, 0);
-                    isAttacking = true;
-                    isStartingAttacking = false;
-                }
-            }
-            else
-            {
-                print("Something is broken in the start attack section");
-            }
+            //Check to see which position the player is at based off the boolean variables
+            if()
         }
 
         //wait to see if player stays in section long enough
@@ -318,7 +153,7 @@ public class BossBehavior : MonoBehaviour
             aIMovement();
 
             //Value Updating based off of map viewfinders
-            /*if (groundView.playerInFrame && rightView.playerInFrame && midView)
+            if (groundView.playerInFrame && rightView.playerInFrame && midView)
             {
                 print("i be seeing where u are, ur at ground");
                 playerAtGround = true;
@@ -409,9 +244,7 @@ public class BossBehavior : MonoBehaviour
                 playerAtRight = false;
                 playerAtLeft = false;
             }
-        */
         }
-        prevXPos = transform.position.x;
     }
 
     //true means fast, false means slow
@@ -457,6 +290,7 @@ public class BossBehavior : MonoBehaviour
             print("made it out of walk");
             anim.SetBool("Walk", false);
         }
+        prevXPos = transform.position.x;
         if (basicAttackViewfinderScript.playerInFrame == true)
         {
             basicAttack = true;
